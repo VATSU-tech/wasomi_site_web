40f8fa75e20140ad4968bb81c81db1d594372066
VATSU_04 <vatsumalambo@gmail.com>
2026-04-23 17:39:20 +0200

    fix: mettre la photo de la pelouse a la premiere place

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -19,7 +19,8 @@ export const galleryCategories = [
 ];
 const s = (l: string) => `../../public/gallerie/${l}`;
 export const galleryItems: GalleryItem[] = [
-  {id: "1", src: u("photo-1551434678-e076c223a692"), title: "Campus principal", category: "Campus", description: "Notre bâtiment historique au cœur de la ville.", featured: true },
+  { id: "13", src: s("campus_1.jpg"), title: "Pelouse centrale", category: "Campus", description: "Espace de détente et d'événements en plein air.", featured: true  },
+  {id: "1", src: u("photo-1551434678-e076c223a692"), title: "Campus principal", category: "Campus", description: "Notre bâtiment historique au cœur de la ville."},
   { id: "2", src: u("photo-1581091226825-a6a2a5aee158"), title: "Projet IA d'étudiants", category: "Réalisations", description: "Hackathon 2024 — équipe primée." },
   { id: "3", src: u("photo-1518770660439-4636190af475"), title: "Laboratoire électronique", category: "Équipement", description: "Stations de prototypage dernière génération." },
   { id: "4", src: u("photo-1522202176988-66273c2fd55f"), title: "Travaux de groupe", category: "Vie étudiante", description: "Espaces collaboratifs ouverts 24/7." },
@@ -31,7 +32,6 @@ export const galleryItems: GalleryItem[] = [
   { id: "10", src: u("photo-1531545514256-b1400bc00f31"), title: "Cafétéria", category: "Vie étudiante" },
   { id: "11", src: u("photo-1606761568499-6d2451b23c66"), title: "Robotique", category: "Réalisations", description: "Compétition régionale 2e place." },
   { id: "12", src: u("photo-1588072432836-e10032774350"), title: "Amphithéâtre", category: "Campus" },
-  { id: "13", src: s("campus_1.jpg"), title: "Pelouse centrale", category: "Campus", description: "Espace de détente et d'événements en plein air." },
   { id: "14", src: s("labo_info.jpg"), title: "Laboratoire informatique moderne", category: "Campus", description: "Laboratoire informatique moderne equiper pour aider les etudiants a faire des realisation en domotique" },
   // { id: "15", src: s("salle.jpg"), title: "Salle de classe moderne", category: "Équipement", description: "Tableaux interactifs et sièges ergonomiques." },
   // { id: "16", src: s("etudiants.jpg"), title: "Vie étudiante animée", category: "Vie étudiante", description: "Clubs, associations et activités sportives." },
